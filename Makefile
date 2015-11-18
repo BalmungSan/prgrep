cba67336a5f2cab590513b9bbbed84168cf59e74
Luis Miguel Mejía Suárez <BalmungSan@localhost.localdomain>
2015-11-18 17:50:59 -0500

    String parameter passing ready

--- Makefile
+++ Makefile
@@ -1,26 +1,29 @@
 prgrep: prgrep.o
-	mv a.out ./prgrep
-	./prgrep "Hello" "He"
+	#./prgrep "Hello" "He"
 
 bm: BoyerMoore.java
 	javac $<
 	java BoyerMoore "jhjhjahshelllskHellolosjsme" "Hello"
 
+test: test.c
+	gcc -g -m32 -o test test.c
+	objdump -d --disassembler-options=intel test &> test.asm	
+
 hello: helloworld.o
-	mv a.out ./hello
 	./hello
 
 read: fileRead.o
-	mv a.out ./read
 	./read hello.txt
 
 %.o:
-	nasm -f elf32 $*.asm
-	ld -m elf_i386 -s $@
+	nasm -f elf -F dwarf -g $*.asm
+	ld -m elf_i386 -o $* $@
 
 clean: 
 	rm -f prgrep
 	rm -f hello
 	rm -f read
+	rm -f test
 	rm -rf *.o
 	rm -rf *.class
+	rm -rf core.*
--- prgrep.asm
+++ prgrep.asm
@@ -1,20 +1,18 @@
 section .data
-  ;Messages for printf
-  msg1 db "The string %s has a length of $d",10,0 ;String and size
+msg: db "Found",10,0
+msgsize: equ $-msg
 
 section .bbs
 
 section .text
-  extern printf ;Use th C function printf
-
   ;Get the length of a string in ecx
   getlen:
     xor edx, edx ;edx = 0 -> length = 0 
     loop: ;For every char in the string
-      cmp byte [ecx + edx], 0 ;If the char is the null ...
-      jz gotlen               ;... we have the length
-      inc edx                 ;... if not, length++ 
-      jmp loop                ;Loop for the next char
+      cmp byte[ecx + edx], 0 ;If the char is the null ...
+      jz gotlen              ;... we have the length
+      inc edx                ;... if not, length++ 
+      jmp loop               ;Loop for the next char
     gotlen: ;Now ecx -> string, edx = lengt
     ret ;return
 
@@ -26,54 +24,58 @@ section .text
     mov ebp, esp
 
     ;Create local variables by reserving space on the stack
-    sub esp, 0x14 ;Reserve space of 20 bytes-- 5 integers
+    sub esp, 0x14 ;Reserve space of 20 bytes -> 5 integers
 
-    ;First variable (boolean result)
-    mov DWORD [edp-4], 0 ;result = 0 = false
+    ;Here we have these variables
+    ;[ebp-4] = boolean result = false (0)
+    ;[ebp-8] = int i = 0
+    ;[ebp-12] = int j = 0
 
-    ;Second variable (int i)
-    mov DWORD [edp-8], 0 ;i = 0
+    ;[ebp-16] = int n
+    mov ecx, [ebp+8]        ;Get text from the stack
+    call getlen             ;Get the length of the string
+    mov dword [ebp-16], edx ;n = edx = text.length
 
-    ;Third variable (int j)
-    mov DWORD [edp-12], 0 ;j = 0
+    mov eax,4
+    mov ebx,1
+    int 80h 
 
-    ;Fourth variable (int n)
-    pop ecx ;Get text from the stack
-    call getlen ;Get the length of the string
-    mov DWORD [edp-16], edx ;n = edx = text.length
+    ;[ebp-20] = int m
+    mov ecx, [ebp+12]       ;Get pattern from the stack
+    call getlen             ;Get the length of the string
+    mov dword [ebp-20], edx ;m = edx = pattern.length 
 
-    push DWORD [edp-16]
-    push ecx 
-    call printf
+    mov eax,4
+    mov ebx,1
+    int 80h
 
-    ;Fifth variable (int m)
-    pop ecx ;Get pattern from the stack
-    call getlen ;Get the length of the string
-    mov DWORD [edp-20], edx ;m = edx = pattern.length 
-
-    push DWORD [edp-20]
-    push ecx
-    call printf
-
-    ;Destroy the stack frame and return
-    mov esp, ebp
-    pop ebp
+    ;return result
+    mov eax, dword [ebp-4]
+    leave
     ret 
 
   ;Main 
   global _start
   _start: 
-    pop ecx ;Get the number of arguments
-    pop ecx ;Get the program name
+    pop ecx ;Get the number of arguments (argc)
 
-    pop ecx ;Get pointer to a new arg (String)
-    cmp ecx, 0 ;If we popped a zero, there isn't more args
-    jz exit
+    pop ecx ;Get the program name (argv[0])
 
-    push  
+    ;Call the search function
+    call search
+
+    ;Check the result value of search
+    add esp,0x10 ;Get the retrun value in eax
+    test eax,eax ;Check if eax == 0 ...
+    jne exit     ;.. if not exit 
+    mov eax,4    ;.. if ture print a message
+    mov ebx,1    
+    mov ecx,msg
+    mov edx,msgsize
+    int 80h
 
   ;Exit function
   exit:
-    mov eax, 1  ;Syscall 1 (exit)
-    mov ebx, 0  ;Exit code 0 (No errors)
-    int 80h     ;Call system
+    mov eax, 0x1  ;Syscall 1 (exit)
+    mov ebx, 0x0  ;Exit code 0 (No errors)
+    int 80h       ;Call system
